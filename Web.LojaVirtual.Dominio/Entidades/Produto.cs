e1f93a6bf6a930b332a0a7f2a86f922db559f962
Jakson de Sousa <jaksonmgs@hotmail.com>
2016-08-20 17:33:35 -0300

    Terceiro Commit

---
+++ Web.LojaVirtual.Dominio/Entidades/Produto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Web.LojaVirtual.Dominio.Entidades
+{
+    public class Produto
+    {
+        public int ProdutoId { get; set; }//Atalho prop
+        public string Nome { get; set; }
+
+        public string Descricao { get; set; }
+
+        public decimal Preco { get; set; }
+        public string Categoria { get; set; }
+    }
+}
