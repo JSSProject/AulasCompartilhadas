94ca352c45dd1761ab684248b8f85968c0a78e7a
Jakson de Sousa <jaksonmgs@hotmail.com>
2016-08-22 22:12:03 -0300

    Sexta Aula

---
+++ Web.LojaVirtual.Dominio/Repositorio/EfDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Data.Entity; //para referenciar a class DbContext
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Web.LojaVirtual.Dominio.Entidades;
+
+namespace Web.LojaVirtual.Dominio.Repositorio
+{
+    public class EfDbContext : DbContext
+    {
+        public DbSet<Produto> Produtos { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder) //Para não fazer a pluralização
+        {
+            modelBuilder.Conventions.Remove < PluralizingTableNameConvention>();
+            modelBuilder.Entity<Produto>().ToTable("Produtos");
+        }
+
+        
+    }
+}
